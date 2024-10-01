726e44fca7967881b28c835234812b3722198ffd
nnoskov <hhb@bk.ru>
2024-10-01 22:00:54 +0300

    Combining task #3 and #4

--- Lesson_2/Task_4/car.js
+++ Lesson_2/Task_4/car.js
@@ -2,6 +2,12 @@ const carName = 'Lexus RX450h';
 let maxSpeed = '200 km/h';
 let carOwner = 'Nikolay';
 
+// Task #3
+alert(`There are one ${carName}`);
+alert(`${carName} has maximum speed - ${maxSpeed}`);
+alert(`${carName} has current owner ${carOwner}`);
+
+// Task #4
 maxSpeed = '150 km/h';
 console.log('У машины изменилась скорость на', maxSpeed);
 
