546e2a34778d2d29589cd3ed9dbc3c04c4641c91
nnoskov <hhb@bk.ru>
2024-10-06 11:06:33 +0300

    Lesson 3 is completed

---
+++ Lesson_4/Task_1/index.js
@@ -0,0 +1,18 @@
+let age = '35';
+let studenSkils = 'high';
+let scool;
+let updateCompleted = true;
+let scoolNumber = 365;
+let studenId = Symbol('Id');
+let competition = null;
+let cryptoWallet = 0xaddfbccdde2342343eeedd4534564356n;
+
+console.log(Number(age), Boolean(age), String(age));
+console.log(Number(studenSkils), Boolean(studenSkils), String(studenSkils));
+console.log(Number(scool), Boolean(scool), String(scool));
+console.log(Number(updateCompleted), Boolean(updateCompleted), String(updateCompleted));
+console.log(Number(scoolNumber), Boolean(scoolNumber), String(scoolNumber));
+//console.log(Number(studenId), Boolean(studenId), String(studenId));
+// Cannot convert a Symbol value to a number
+console.log(Number(competition), Boolean(competition), String(competition));
+console.log(Number(cryptoWallet), Boolean(cryptoWallet), String(cryptoWallet));
